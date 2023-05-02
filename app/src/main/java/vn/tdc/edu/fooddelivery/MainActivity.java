72b1a497c4caeceafc2e088bd5845f621094e746
Nguyen Nhat Truong <nhattruongnguyenag@gmail.com>
2023-05-02 20:08:37 +0700

    demo-display-image-from-url

--- app/src/main/java/vn/tdc/edu/fooddelivery/MainActivity.java
+++ app/src/main/java/vn/tdc/edu/fooddelivery/MainActivity.java
@@ -3,12 +3,27 @@ package vn.tdc.edu.fooddelivery;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView imageView;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.imageView);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(view -> {
+            Glide.with(this).load("https://www.freepnglogos.com/uploads/android-logo-png/android-logo-0.png")
+                    .into(imageView);
+        });
     }
 }
