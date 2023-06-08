135739b10e3de5cfbf0c168c9bab199070f2d435
SonvanJune <sonthu3333@gmail.com>
2023-06-08 17:44:34 +0700

    .

--- app/src/main/java/vn/tdc/edu/fooddelivery/constant/SystemConstant.java
+++ app/src/main/java/vn/tdc/edu/fooddelivery/constant/SystemConstant.java
@@ -7,5 +7,5 @@ public class SystemConstant {
     public static final String API_BASE_URL = SERVER_IP + "/food-delivery/app/public/";
     public static final String IMAGES_BASE_URL = SERVER_IP + "/photo/";
     public static final String MSG_WRONG_PASSWORD = "sai passsword";
-    public static final String MSG_USER_NOT_EXISTS = "Khong tim thay user";
+    public static final String MSG_USER_NOT_EXISTS = "Không tìm thấy user";
 }
