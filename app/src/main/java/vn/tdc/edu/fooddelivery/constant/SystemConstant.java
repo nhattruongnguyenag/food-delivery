534a1ef16b526e0982fe705f2f69d547bb64f0f5
SonvanJune <sonthu3333@gmail.com>
2023-06-09 11:54:05 +0700

    notifycation of register view

--- app/src/main/java/vn/tdc/edu/fooddelivery/constant/SystemConstant.java
+++ app/src/main/java/vn/tdc/edu/fooddelivery/constant/SystemConstant.java
@@ -3,7 +3,7 @@ package vn.tdc.edu.fooddelivery.constant;
 
 public class SystemConstant {
     //public static final String SERVER_IP = "http://nhattruongnguyen.com";
-    public static final String SERVER_IP = "http://192.168.0.104";
+    public static final String SERVER_IP = "http://192.168.1.11";
     public static final String API_BASE_URL = SERVER_IP + "/food-delivery/app/public/";
     public static final String IMAGES_BASE_URL = SERVER_IP + "/photo/";
     public static final String MSG_WRONG_PASSWORD = "sai passsword";
